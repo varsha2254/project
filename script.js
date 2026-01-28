c89530b97185b3530718817da679047b83d060a6
varsha2254 <saivarsh2220@gmail.com>
2026-01-28 10:50:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelectorAll(".gallery img").forEach(img => {
-  img.addEventListener("click", () => {
-    const popup = document.createElement("div");
-    popup.className = "lightbox";
-    popup.innerHTML = `<img src="${img.src}">`;
-    popup.onclick = () => popup.remove();
-    document.body.appendChild(popup);
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("toggleTheme");
+
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    toggle.textContent =
+      document.body.classList.contains("dark") ? "☀️" : "🌙";
   });
 });
